a0cd6e9b42122856c76d283acbe50e91fe6672cb
yasinakcil <68876726+yasinakcilbmt@users.noreply.github.com>
2025-05-16 18:39:08 +0300

    Proje dosyalarını ekle

--- src/main/java/com/jvt/timetracker/service/WorkLogService.java
+++ src/main/java/com/jvt/timetracker/service/WorkLogService.java
@@ -1,7 +1,14 @@
 package com.jvt.timetracker.service;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.jvt.timetracker.model.Project;
+import com.jvt.timetracker.model.User;
+import com.jvt.timetracker.model.WorkLog;
 import com.jvt.timetracker.repository.*;
 
 import jakarta.transaction.Transactional;
@@ -13,6 +20,24 @@ import lombok.RequiredArgsConstructor;
 public class WorkLogService {
     private final WorkLogRepository workLogRepository;
     private final ProjectRepository projectRepository;
+
+    public WorkLog clockIn(Long projectId){
+
+        Project project = projectRepository.findById(projectId)
+        .orElseThrow(() -> new RuntimeException("Project not found"));
+
+        WorkLog workLog = new WorkLog();
+        workLog.setProject(project);
+        workLog.setStartTime(OffsetDateTime.now());
+        return workLogRepository.save(workLog);
+    }
+
+
+    
+        
+
+
+    
 
     
 
